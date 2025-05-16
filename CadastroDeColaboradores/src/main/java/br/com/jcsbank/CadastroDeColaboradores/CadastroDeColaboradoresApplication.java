ac914e0bd8a5f4a3439f4fa588cf4e2111cf6009
DevJoseSarmento <dev_jose_sarmento@icloud.com>
2025-05-16 20:51:33 -0300

    Initial commit

---
+++ CadastroDeColaboradores/src/main/java/br/com/jcsbank/CadastroDeColaboradores/CadastroDeColaboradoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcsbank.CadastroDeColaboradores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeColaboradoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeColaboradoresApplication.class, args);
+	}
+
+}
---
+++ CadastroDeColaboradores/src/test/java/br/com/jcsbank/CadastroDeColaboradores/CadastroDeColaboradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcsbank.CadastroDeColaboradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeColaboradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
